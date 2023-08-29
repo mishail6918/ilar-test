5ad6342f5b31c734b5d27cb2591a95410319ec39
Mikhail Kozlov <gamestar595@gmail.com>
2023-08-29 16:04:33 +0300

    Likes counter fix

--- src/js/components/comment-counter.js
+++ src/js/components/comment-counter.js
@@ -1,20 +1,18 @@
 const btns = document.querySelectorAll('.likes-counter__btn');
-let likeCountPerUser = 0;
-btns.forEach((element) =>{
+btns.forEach((element, index) =>{
     element.addEventListener('click', function () {
-        if (this.classList.contains('up') && likeCountPerUser === 0) {
+        let checkClass = element.parentNode.querySelector('.likes-counter__number').classList.contains('count-active');
+        if (this.classList.contains('up') && checkClass === false) {
             let count = element.parentNode.querySelector('.likes-counter__number').innerText;
             Number(count++);
             element.parentNode.querySelector('.likes-counter__number').innerText = count;
             element.parentNode.querySelector('.likes-counter__number').classList.add('count-active');
-            likeCountPerUser++;
         }
-        else if(this.classList.contains('down') && likeCountPerUser === 1) {
+        else if(this.classList.contains('down') && checkClass) {
             let count = element.parentNode.querySelector('.likes-counter__number').innerText;
             Number(count--);
             element.parentNode.querySelector('.likes-counter__number').innerText = count;
             element.parentNode.querySelector('.likes-counter__number').classList.remove('count-active');
-            likeCountPerUser--;
         }
     })
 });
